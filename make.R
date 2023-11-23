9a26acbfd6b565874c7a1341f645fa0a74500f5a
Soumaya Belghali <soumaya.belghali@cefe.cnrs.fr>
2023-11-23 14:04:25 +0100

    primeiro commit

---
+++ make.R
@@ -0,0 +1,35 @@
+# ## Ajout d'un fichier DESCRIPTION ----
+# rcompendium::add_description()
+# ## Ajout d'une licence ----
+# rcompendium::add_license(license = "GPL-2")
+# ## Ajout de sous-répertoires ----
+# rcompendium::add_compendium(compendium = c("data", "analyses", "R", "figures", "outputs"))
+targets::tar_config_set(
+  store = "outputs/pipeline/",
+  script = "analyses/pipeline.R"
+)
+
+## Génération de la doc ----
+devtools::document()
+
+## Ajout de dépendances ----
+rcompendium::add_dependencies(compendium = ".")
+
+## Ajout d'un README ----
+rcompendium::add_readme_qmd(type = "compendium")
+## Conversion du README.Rmd en README.md ----
+quarto::render("README.Qmd")
+
+
+
+## Installation des packages manquants ----
+remotes::install_deps(upgrade = "never")
+
+## Chargement des packages et fonctions R ----
+devtools::load_all()
+
+targets::tar_make()
+targets::tar_visnetwork()
+
+
+
